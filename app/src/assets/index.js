9437bb65b61d488782b8644fbe331f00ddef5d09
junukim <junukim.dev@gmail.com>
2020-06-09 09:45:51 +0900

    [CRETEA] add assets

---
+++ app/src/assets/index.js
@@ -0,0 +1,7 @@
+import AttendanceActive from './attendance_active.png';
+import Attendance from './attendance.png';
+import CouponActive from './coupon_active.png';
+import Coupon from './coupon.png';
+import Logo from './logo.png';
+
+export {AttendanceActive, Attendance, CouponActive, Coupon, Logo};
