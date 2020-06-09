8cb7887b52b5cd8f2a70994106acf1750108695a
junukim <junukim.dev@gmail.com>
2020-06-09 09:47:05 +0900

    [CREATE] main page

---
+++ app/src/component/main/attendanceTab/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AttendanceTab = ({navigation}) => {
+  return (
+    <View>
+      <Text>asdf</Text>
+    </View>
+  );
+};
+
+export default AttendanceTab;
