869bd45a1f1768c6a3e011ba0626862730abf294
junukim <junukim.dev@gmail.com>
2020-06-29 08:48:30 +0900

    [UPDATE] images

--- app/src/assets/index.js
+++ app/src/assets/index.js
@@ -3,5 +3,17 @@ import Attendance from './attendance.png';
 import CouponActive from './coupon_active.png';
 import Coupon from './coupon.png';
 import Logo from './logo.png';
+import PingCouponLoginBg from './pingCouponLoginBg.png';
+import LeftArrow from './left_arrow.png';
+import Search from './search.png';
 
-export {AttendanceActive, Attendance, CouponActive, Coupon, Logo};
+export {
+  AttendanceActive,
+  Attendance,
+  CouponActive,
+  Coupon,
+  Logo,
+  PingCouponLoginBg,
+  LeftArrow,
+  Search,
+};
